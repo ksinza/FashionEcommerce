d1f46536f13208fe3864b785f36a6d516bdb8785
Kevin Sinza <ksinza@squareballstudios.com>
2021-10-20 17:04:48 -0500

    upload resource and assets

--- FashionEccomerce/FashionEccomerce.iOS/AppDelegate.cs
+++ FashionEccomerce/FashionEccomerce.iOS/AppDelegate.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using CarouselView.FormsPlugin.iOS;
 using Foundation;
 using UIKit;
 
@@ -22,6 +22,7 @@ namespace FashionEccomerce.iOS
         //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
+            CarouselViewRenderer.Init();
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
 
